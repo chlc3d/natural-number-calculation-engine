cb1184a1be1af34fb9d4b89279fc9894157fcb25
trs <timothy.stamper@gmail.com>
2014-11-09 15:35:43 -0600

    Added instrumentation

--- nat.py
+++ nat.py
@@ -6,65 +6,104 @@ EOF = 0
 class State:
 	def __init__(self, **kwargs):
 		self.memory = kwargs['memory']
-		self.pc = kwargs['pc']
+		self.pc = 0
 		self.tape = kwargs['tape']
+		self.output = ''
+		self.head_moves = 0
 
-def get(mem, pc):
-	return mem.get(pc, 0)
+		self.executed_instructions = {
+			"incr": 0,
+			"decr": 0,
+			"goto": 0,
+			"copy": 0,
+			"inpt": 0,
+			"outp": 0,
+			"outpd": 0,
+			"number": 0
+		}
 
-def get_int(mem, pc):
-	val = get(mem, pc)
-	assert(type(val) is int)
-	return val
+	def get(self):
+		return self.memory.get(self.pc, 0)
+
+	def get_int(self):
+		val = self.get()
+		assert(type(val) is int)
+		return val
+
+	def set(self, val):
+		self.memory[self.pc] = val
+
+	def next(self):
+		self.pc += 1
+		self.head_moves +=1
+
+	def jump(self, pc):
+		self.head_moves += abs(self.pc - pc)
+		self.pc = pc
+
+
+
+#def get(mem, pc):
+	#return mem.get(pc, 0)
+#
+#def get_int(mem, pc):
+	#val = get(mem, pc)
+	#assert(type(val) is int)
+	#return val
 
 def _incr(state):
-	val = get_int(state.memory, state.pc+1)
-	state.memory[state.pc+1] = val + 1
-	state.pc += 1
+	state.next()
+	state.set(state.get_int() + 1)
 
 def _decr(state):
 	#print '---'
 
 	#print state.pc+1
 	#print state.memory[state.pc+1]
-	val = get_int(state.memory, state.pc+1)
+	state.next()
+	val = state.get_int()
 	if val <= 0:
 		#go to trap
-		state.pc = 98
+		state.jump(98)
 	else:
-		state.memory[state.pc+1] = val - 1
-		state.pc += 1
+		state.set(val -1)
 
 def _goto(state):
-	val = get_int(state.memory, state.pc+1)
-	state.pc = val
+	state.next()
+	state.jump(state.get_int())
 
 def _copy(state):
-	src_addr = get_int(state.memory, state.pc+1)
-	dest_addr = get_int(state.memory, state.pc+2)
-	state.memory[dest_addr] = get(state.memory, src_addr)
+	orig_addr = state.pc
 
-	#if dest_addr > max_state.pc:
-		#max_state.pc = dest_addr
+	state.next()
+	src_addr = state.get_int()
+	state.next()
+	dest_addr = state.get_int()
 
-	state.pc += 1
+	state.jump(src_addr)
+	value = state.get()
+
+	state.jump(dest_addr)
+	state.set(value)
+
+	state.jump(orig_addr + 3)
 
 def _user_input(state):
+	state.next()
 	if state.tape == "":
-		state.memory[state.pc+1] = EOF
+		state.set(EOF)
 	else:
-		state.memory[state.pc+1] = ord(state.tape[0])
+		state.set(ord(state.tape[0]))
 		state.tape = state.tape[1:]
 
-	state.pc += 1
 
 def _output(state):
-	sys.stdout.write(chr(get(state.memory, state.pc + 1)))
-	state.pc += 1
+	state.next()
+	state.output += chr(state.get())
 
 def _debug_output(state):
-	print "%s: %s" % (state.pc, get(state.memory, state.pc+1))
-	state.pc += 1
+	state.next()
+	state.output += "%s: %s\n" % (state.pc, state.get())
 
 def interpret(debug, program_path, input_files):
 
@@ -77,7 +116,7 @@ def interpret(debug, program_path, input_files):
 
 	print ':'.join(x.encode('hex') for x in user_input)
 
-	state = State(memory=dict(), tape=user_input, pc=0)
+	state = State(memory=dict(), tape=user_input)
 
 	for line in program:
 		line = ''.join(line.split())
@@ -104,15 +143,19 @@ def interpret(debug, program_path, input_files):
 
 	while state.pc <= max_pc:
 
-		instr = get(state.memory, state.pc)
+		instr = state.get()
 
 		#print "%s: %s (next: %s)" % (state.pc, instr, get(state.memory,state.pc+1))
 		#print state.pc
 		#print instr
 		if type(instr) is int:
-			state.pc += 1
+			state.next()
+			state.executed_instructions['number'] += 1
 			continue
 
+		instr = instr.lower()
+
+		state.executed_instructions[instr] += 1
 		{
 			"incr": _incr,
 			"decr": _decr,
@@ -121,12 +164,18 @@ def interpret(debug, program_path, input_files):
 			"inpt": _user_input,
 			"outp": _output,
 			"outpd": _debug_output
-		}[get(state.memory, state.pc).lower()](state)
+		}[instr](state)
 
-	print ""
-	print "final state.pc: %s" % state.pc
+
 	if debug:
 		print "final state.memory: %s" % sorted(state.memory.items())
+	return state
 
 if __name__ == '__main__':
-	interpret(False, sys.argv[1], sys.argv[2:])
+	state = interpret(False, sys.argv[1], sys.argv[2:])
+	print state.output
+
+	print ""
+	print state.executed_instructions
+	print "head moves: %s" % state.head_moves
+	print "final pc: %s" % state.pc
