457da713889098541b52d3058a655e126d8977a5
trs <timothy.stamper@gmail.com>
2014-11-09 18:39:37 -0600

    cleaned up tests

--- bf_test.py
+++ bf_test.py
@@ -9,12 +9,17 @@ def print_state(st):
 	print "head moves: %s" % st.head_moves
 	print "final pc: %s" % st.pc
 
+print "bfi.txt hello.bf:"
+print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/hello/hello.bf']))
 
-print "compare cat.nn to brainfuck cat interpret!"
+print "bfi.txt cat.bf catinput.txt"
 print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/cat/cat.bf',
 								'brainfuck/test/cat/catinput.txt']))
 
+print "cat.nn catinput.txt"
 print_state(nat.interpret(False, 'examples/cat.nn', ['brainfuck/test/cat/catinput.txt']))
 
+print "simple.nn catinput.txt"
+print_state(nat.interpret(False, 'simple.nn', ['brainfuck/test/cat/catinput.txt']))
 
 
