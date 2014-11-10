5796716c47b94575ff4c2fbd6a1a9ca7eeee9f7a
trs <timothy.stamper@gmail.com>
2014-11-09 19:55:18 -0600

    added more instrumentation.  From now on I'll use 'optimize' and 'breaksprofiling' for anything that is designed to improve performance or change the format of profiler output

--- bf_test.py
+++ bf_test.py
@@ -8,18 +8,25 @@ def print_state(st):
 	print "total instructions: %s" % st.num_instructions_executed()
 	print "head moves: %s" % st.head_moves
 	print "final pc: %s" % st.pc
+	print ""
 
 print "bfi.txt hello.bf:"
 print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/hello/hello.bf']))
 
-print "bfi.txt cat.bf catinput.txt"
+print "bfi.txt cat.bf catinput.txt:"
 print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/cat/cat.bf',
 								'brainfuck/test/cat/catinput.txt']))
 
-print "cat.nn catinput.txt"
+print "bfi.txt rot13.bf rot13input.txt:"
+print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/cristofd/rot13.bf', 'brainfuck/test/cristofd/rot13input.txt']))
+
+print "bfi.txt stress1.bf:"
+print_state(nat.interpret(False, 'bfi.txt', ['brainfuck/test/cristofd/stress1.bf']))
+
+print "cat.nn catinput.txt:"
 print_state(nat.interpret(False, 'examples/cat.nn', ['brainfuck/test/cat/catinput.txt']))
 
-print "simple.nn catinput.txt"
+print "simple.nn catinput.txt:"
 print_state(nat.interpret(False, 'simple.nn', ['brainfuck/test/cat/catinput.txt']))
 
 
---
+++ brainfuck/test/cristofd/readme.txt
@@ -0,0 +1,10 @@
+Borrowed and modified from http://www.hevanet.com/cristofd/brainfuck/
+
+largecell.bf
+============
+Prints a "#" at 30000
+
+stress1.bf
+==========
+Tests for several obscure problems. Should output an H.
+
