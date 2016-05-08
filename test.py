368561d45e75a6e52a07f8c531a3b96d5294f380
trstamper <timothy.stamper@gmail.com>
2016-05-08 13:07:10 -0500

    Organized directory structure.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import nn_interpreter
-import nnp_compiler
+import nn_interpreter.nn_interpreter as nn_interpreter
+import nnp_compiler.nnp_compiler as nnp_compiler
+import os
 
 def print_state(st):
 	print st.output
@@ -11,25 +12,35 @@ def print_state(st):
 	print "final pc: %s" % st.pc
 	print ""
 
-bfi_compiled = nnp_compiler.compile_file('brainfuck/bfi.nnp')
+
+bf_dir = 'examples/brainfuck'
+bf_test_dir = 'examples/brainfuck/test'
+
+bfi_compiled = nnp_compiler.compile_file(os.path.join(bf_dir, 'bfi.nnp'))
 
 print "bfi.nnp hello.bf:"
-print_state(nn_interpreter.interpret(False, bfi_compiled, ['brainfuck/test/hello/hello.bf']))
+print_state(nn_interpreter.interpret(False, bfi_compiled, [os.path.join(bf_test_dir, 'hello/hello.bf')]))
 
 print "bfi.nnp cat.bf catinput.txt:"
-print_state(nn_interpreter.interpret(False, bfi_compiled, ['brainfuck/test/cat/cat.bf',
-								'brainfuck/test/cat/catinput.txt']))
+print_state(nn_interpreter.interpret(False, bfi_compiled,
+	[os.path.join(bf_test_dir, 'cat/cat.bf'),
+	os.path.join(bf_test_dir, 'cat/catinput.txt')]))
 
 print "bfi.nnp rot13.bf rot13input.txt:"
-print_state(nn_interpreter.interpret(False, bfi_compiled, ['brainfuck/test/cristofd/rot13.bf', 'brainfuck/test/cristofd/rot13input.txt']))
+print_state(nn_interpreter.interpret(False, bfi_compiled,
+	[os.path.join(bf_test_dir, 'cristofd/rot13.bf'),
+	os.path.join(bf_test_dir, 'cristofd/rot13input.txt')]))
 
 print "bfi.nnp stress1.bf:"
-print_state(nn_interpreter.interpret(False, bfi_compiled, ['brainfuck/test/cristofd/stress1.bf']))
+print_state(nn_interpreter.interpret(False, bfi_compiled,
+	[os.path.join(bf_test_dir, 'cristofd/stress1.bf')]))
 
 print "cat.nn catinput.txt:"
-print_state(nn_interpreter.interpret_file(False, 'examples/cat.nn', ['brainfuck/test/cat/catinput.txt']))
+print_state(nn_interpreter.interpret_file(False, 'examples/cat.nn',
+	[os.path.join(bf_test_dir, 'cat/catinput.txt')]))
 
 print "simple.nn catinput.txt:"
-print_state(nn_interpreter.interpret_file(False, 'examples/simple.nn', ['brainfuck/test/cat/catinput.txt']))
+print_state(nn_interpreter.interpret_file(False, 'examples/simple.nn',
+	[os.path.join(bf_test_dir, 'cat/catinput.txt')]))
 
 
